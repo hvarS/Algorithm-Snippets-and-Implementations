199ab3c527bbd34bffa97c3b92cab400e4f35339
hvarS <34151780+hvarS@users.noreply.github.com>
2020-12-22 19:40:53 +0530

    GCD , Modular Algebra , General Number Theory

--- Number Theory/modinverse.cpp
+++ Number Theory/modinverse.cpp
@@ -3,6 +3,32 @@ using namespace std;
 
 typedef long long ll;
 
+int d, x, y;
+
+void exteuclid(int A, int B)
+{
+    if (B == 0)
+    {
+        d = A;
+        x = 1;
+        y = 0;
+    }
+    else
+    {
+        exteuclid(B, A % B);
+        int temp = x;
+        x = y;
+        y = temp - (A / B) * y;
+    }
+}
+
+int modinverse(int A, int M)
+{
+    exteuclid(A, M);
+    return (x + M) % M;
+}
+
 int main()
 {
+    cout << modinverse(5, 12) << endl;
 }
